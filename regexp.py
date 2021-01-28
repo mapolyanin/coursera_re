9a9b303599af404398534d516c2c76baf4eb62b4
mi.al.pol <mi.al.pol@gmail.com>
2021-01-28 23:11:07 +0800

    not work

--- regexp.py
+++ regexp.py
@@ -1,7 +1,8 @@
 def calculate(data, findall):
-    matches = findall(r"")  # Если придумать хорошую регулярку, будет просто
+    matches = findall(r"([abc])([-+]?)=([abc]?)([+-]?\d*)")  # Если придумать хорошую регулярку, будет просто
+    #print(matches, "\n")
     for v1, s, v2, n in matches:  # Если кортеж такой структуры: var1, [sign]=, [var2], [[+-]number]
         # Если бы могло быть только =, вообще одной строкой все считалось бы, вот так:
-        data[v1] = data.get(v2, 0) + int(n or 0)
-
+        data[v1] = (-data.get(v2, 0) if s=="-" else data.get(v2, 0))   + int(n or 0)
+        print (v1, s, v2, n, "\n")
     return data
